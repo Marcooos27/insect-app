a9c4fc641e14dabb6af786a7d2f0f64a6ec9e52d
Marcooos27 <marcosrecuencodemoya@gmail.com>
2026-04-09 11:46:44 +0200

    subiendo el archivo de update_password

---
+++ backend/scripts/update_password.py
@@ -0,0 +1,21 @@
+from backend.database import get_connection
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+conn = get_connection()
+cur = conn.cursor()
+
+nueva_password = "la_password_que_quieras"
+nuevo_hash = pwd_context.hash(nueva_password[:72])
+
+
+#AQUI PONGO EL EMAIL DEL QUE QUIERO CAMBIAR LA CONTRASEÑA
+cur.execute(
+    "UPDATE usuarios SET password_hash = %s WHERE email = %s",
+    (nuevo_hash, "marcos@insecteat.es") 
+)
+conn.commit()
+cur.close()
+conn.close()
+print("Contraseña actualizada")
